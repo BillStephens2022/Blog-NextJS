850b7dbce5bb1276e878cf72091a791808682cb6
BillStephens2022 <two4onebill@yahoo.com>
2023-10-12 16:01:00 -0400

    added functionality to add a blog post to mongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -18,11 +20,37 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+
+    try {
+      const pwParam = encodeURIComponent(process.env.MONGODB_PW);
+      client = await MongoClient.connect(
+        `mongodb+srv://two4onebill:${pwParam}@cluster0.xvwjnur.mongodb.net/blog-nextjs?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database..." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" })
+      return;
+    }
+
+    
 
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
+
+      client.close();
   }
 }
 
