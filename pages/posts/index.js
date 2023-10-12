a9682595d0e66f1f935d6bbb530c93f96a8851d9
BillStephens2022 <two4onebill@yahoo.com>
2023-10-12 13:55:04 -0400

    used getAllPosts helper function to fetch all posts for the all posts page using getStaticProps

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,43 +1,20 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-nextjs",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-      date: "2023-02-14",
-    },
-    {
-      slug: "getting-started-with-nextjs2",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-      date: "2023-02-14",
-    },
-    {
-      slug: "getting-started-with-nextjs3",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-      date: "2023-02-14",
-    },
-    {
-      slug: "getting-started-with-nextjs4",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is a React framework for Production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
-      date: "2023-02-14",
-    },
-  ];
 
-function AllPostsPage() {
 
-    return <AllPosts posts={DUMMY_POSTS} />
+function AllPostsPage(props) {
+
+    return <AllPosts posts={props.posts} />
+}
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts
+    }
+  };
 }
 
 export default AllPostsPage;
