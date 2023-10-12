9505187967d5917d090cb10cd1b413ab3686a704
BillStephens2022 <two4onebill@yahoo.com>
2023-10-12 14:55:37 -0400

    updated styling of markdown file

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
@@ -21,20 +23,32 @@ function PostContent(props) {
     // },
     p(paragraph) {
       const { node } = paragraph;
-      if (node.children[0].tagName === 'img') {
+      if (node.children[0].tagName === "img") {
         const image = node.children[0];
 
-        return (<div className={classes.image}>
-          <Image
-          src={`/images/posts/${post.slug}/${image.properties.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-        </div>
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
         );
       }
-      return <p>{paragraph.children}</p>
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];  // className is language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
